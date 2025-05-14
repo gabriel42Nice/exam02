d330b02e3f983ed42b29223db06c85f486cd54e2
gchinaul <gabriel.chinault@outlook.com>
2025-05-14 13:49:25 +0200

    Update rev_wstr.c

--- exam02/lvl 4/rev_wstr.c
+++ exam02/lvl 4/rev_wstr.c
@@ -1,51 +1,32 @@
 #include <unistd.h>
 
-int str_length(char *str)
+int	main(int ac, char **av)
 {
-    int len = 0;
-    while (*str)
-    {
-        str++;
-        len++;
-    }
-    return (len);
+	int i;
+	int j;
+	int first_word;
+
+	i = 0;
+	first_word = 0;
+	if (ac == 2)
+	{
+		while (av[1][i] != '\0')
+			i++;
+		i--;
+		while (i >= 0)
+		{
+			while (i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
+				i--;
+			j = i;
+			while (j >= 0 && av[1][j] != ' ' && av[1][j] != '\t')
+				j--;
+			if (first_word)
+				write(1, " ", 1);
+			write(1, &av[1][j + 1], i - j);
+			first_word = 1;
+			i = j;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
-
-int space(char chr)
-{
-    return (chr == ' ' || chr == '\t');
-}
-
-void print_reverse(char *str)
-{
-    int fast = str_length(str) - 1;
-    int slow = fast;
-    int curr = fast;
-
-    while (fast >= 0)
-    {
-        slow = fast;
-        curr = fast;
-        while (fast >= 0 && !space(str[fast]))
-            fast--;
-        fast++;
-        curr = fast;
-        while (slow >= curr)
-        {
-            write(1, &str[curr], 1);
-            curr++;
-        }
-        if (fast > 0)
-            write(1, " ", 1);
-        fast -= 2;
-    }
-}
-
-int main(int ac, char **av)
-{
-    if (ac == 2)
-        print_reverse(av[1]);
-    write(1, "\n", 1);
-    return (1);
-}
-
