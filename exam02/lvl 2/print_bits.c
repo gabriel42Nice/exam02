3890ce3088330282560adb83248901c578f33dbb
gchinaul <gabriel.chinault@outlook.com>
2025-05-14 13:55:55 +0200

    Update print_bits.c

--- exam02/lvl 2/print_bits.c
+++ exam02/lvl 2/print_bits.c
@@ -12,16 +12,3 @@ void print_bit(unsigned char octet)
         write (1, &bit, 1);
     }
 }
-
-/*----------------------------------version 2 --------------------------------*/
-void print_bit(unsigned char octet)
-{
-    int i = 8;
-    unsigned char bit = 0;
-
-    while (i--)
-    {
-        bit = (octet >> i & 1) + '0';
-        write (1, &bit, 1);
-    }
-}
